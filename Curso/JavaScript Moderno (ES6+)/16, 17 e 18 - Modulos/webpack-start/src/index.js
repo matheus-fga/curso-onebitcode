8c0e5a594fd63bdc1045285839401115dbb7978a
Matheus Silva <matheussilva.fga@gmail.com>
2022-02-23 15:20:37 -0300

    including classes on exporting and importing modules with CommonJS

---
+++ Curso/JavaScript Moderno (ES6+)/16, 17 e 18 - Modulos/webpack-start/src/index.js
@@ -0,0 +1,12 @@
+const Planet = require("./planet")
+const solarSystem = require("./solar_system")
+
+const earth = new Planet("Terra", 50100000)
+earth.rotate()
+
+solarSystem.planets.push(earth)
+solarSystem.planets.push(new Planet("Marte", 144800000))
+solarSystem.planets.push(new Planet("Mercurio", 74800000))
+solarSystem.planets.push(new Planet("Saturno", 4270000000))
+
+console.log(solarSystem)
