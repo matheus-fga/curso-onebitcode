65ec78064da2a2c285c6557ee738cbe0480a53ee
Matheus Silva <matheussilva.fga@gmail.com>
2022-02-23 12:05:02 -0300

    including webpack usage classes

---
+++ Curso/JavaScript Moderno (ES6+)/13, 14 e 15 - Utilizando Webpack/src/index.js
@@ -0,0 +1,3 @@
+import moment from "moment/src/moment"
+
+alert(`Hora Atual: ${moment().format("HH:mm:ss")}`)
---
+++ Curso/JavaScript Moderno (ES6+)/13, 14 e 15 - Utilizando Webpack/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path")
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    galaxy: './src/galaxy.js'
+  },
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+
+  watch: true,
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+      watch: true,
+    }
+  }
+}
