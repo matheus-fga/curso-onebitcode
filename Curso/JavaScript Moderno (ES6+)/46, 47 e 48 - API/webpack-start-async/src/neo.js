c843acdc4496ec0751834c3aa902519a6aed5c47
Matheus Silva <matheussilva.fga@gmail.com>
2022-03-03 10:30:37 -0300

    including API classes

---
+++ Curso/JavaScript Moderno (ES6+)/46, 47 e 48 - API/webpack-start-async/src/neo.js
@@ -0,0 +1,8 @@
+export default class Neo {
+  constructor(id, name, averageDiameter, isSentry) {
+    this.id = id
+    this.name = name
+    this.averageDiameter = averageDiameter
+    this.isSentry = isSentry
+  }
+}
