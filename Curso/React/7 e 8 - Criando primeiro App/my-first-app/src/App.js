506eb54ff7ca671a8a037b1041b3ff5f74f367b3
Matheus Silva <matheussilva.fga@gmail.com>
2022-03-04 09:47:24 -0300

    Including class creating first component

---
+++ Curso/React/7 e 8 - Criando primeiro App/my-first-app/src/App.js
@@ -0,0 +1,15 @@
+import HelloWorld from './Hello'
+import { HelloWorld2 } from './Hello'
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld/>
+      <p>
+        <HelloWorld2/>
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ Curso/React/7 e 8 - Criando primeiro App/my-first-app/src/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HelloWorld = () => {
+  return <h1>Hello World!</h1>;
+}
+
+export class HelloWorld2 extends React.Component {
+  render() {
+    return <h1>Hello World 2!</h1>;
+  }
+}
+
+export default HelloWorld;
