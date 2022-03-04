4d160b762ea5556b47b2e2d66fd9912343701b0f
Matheus Silva <matheussilva.fga@gmail.com>
2022-03-04 10:13:11 -0300

    Including lesson create lorem ipsum app

---
+++ Curso/React/9 - Exercicio Lorem Ipsum/lorem-ipsum-component/src/App.js
@@ -0,0 +1,13 @@
+import { LoremIpsum } from './components/LoremIpsum';
+import Hello from './components/Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello/>
+      <LoremIpsum/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Curso/React/9 - Exercicio Lorem Ipsum/lorem-ipsum-component/src/components/LoremIpsum.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const LoremIpsum = () => {
+  return <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem voluptatibus debitis atque esse, laudantium recusandae voluptas vitae nulla eos minus, voluptate quo ducimus 
+            fuga enim eaque porro eum explicabo doloremque!</p>
+}
