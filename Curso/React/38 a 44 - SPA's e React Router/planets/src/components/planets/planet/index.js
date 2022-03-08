d9c4d11e0487c96804f9c810f76c228adb713932
Matheus Silva <matheussilva.fga@gmail.com>
2022-03-08 15:21:03 -0300

    including create new screen class

--- Curso/React/38 a 44 - SPA's e React Router/planets/src/components/planets/planet/index.js
+++ Curso/React/38 a 44 - SPA's e React Router/planets/src/components/planets/planet/index.js
@@ -1,44 +1,13 @@
-import React, { useState, useEffect} from 'react';
+import React from 'react';
 import GreyImg from '../../shared/grey_img';
 import DescriptionWithLink from '../../shared/description_with_link';
-import Form from './forms';
 
 const Planet = (props) => {
-  const [satellites, setSatellites] = useState([])
-
-  useEffect(() => {
-    const getSatellites = async () => {
-      const response = await fetch(`http://localhost:3000/api/${props.id}.json`)
-      const data = await response.json()
-      setSatellites(data['satellites'])
-    }
-    getSatellites()
-  }, [props.id])
-
-  const addNewSatellite = (newSatellite) => {
-    setSatellites([...satellites, newSatellite])
-  }
-  
-  function ListSatellites(props) {
-    return <li>{props.name}</li>
-  }
-  
-  const satellitesList = satellites.map(satellite => 
-      <ListSatellites key={satellite.name.toString()} name={satellite.name}/>
-  )
 
   return(
     <div>
       <DescriptionWithLink name={props.name} description={props.description} href={props.href}/>
       <GreyImg img_url={props.img_url}/>
-      <h4>Satélites</h4>
-      <hr />
-      <Form addNewSatellite={addNewSatellite}/>
-      <hr />
-      <ul>
-        {satellitesList}
-      </ul>
-      <hr />
     </div>
   )
 }
