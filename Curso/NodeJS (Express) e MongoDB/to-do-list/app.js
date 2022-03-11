4d183c05ddfcade818f682088ed1d87d4d868a4a
Matheus Silva <matheussilva.fga@gmail.com>
2022-03-11 11:18:29 -0300

    Creating file to concentrate routes with Express Router

--- Curso/NodeJS (Express) e MongoDB/to-do-list/app.js
+++ Curso/NodeJS (Express) e MongoDB/to-do-list/app.js
@@ -1,24 +1,12 @@
 const express = require('express');
+const checklistRouter = require('./src/routes/checklist');
 
 const app = express();
 
 app.use(express.json());
 
-const log = (req, res, next) => {
-  console.log(req.body);
-  console.log(`Data atual: ${Date.now()}`);
-  next();
-}
-
-app.use(log)
-
-app.get('/', (req, res) => {
-  res.send("<h1>Minha Lista de Tarefas</h1>");
-})
-
-app.get('/json', (req, res) => {
-  res.json({title: 'Tarefa X', done: true});
-})
+// Routes 
+app.use('/checklists', checklistRouter);
 
 app.listen(3000, () => {
   console.log('Servidor foi iniciado!');
