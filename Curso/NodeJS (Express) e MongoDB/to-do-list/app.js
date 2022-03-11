36a642785eeb628fa37fed11902417f0cc90d9b0
Matheus Silva <matheussilva.fga@gmail.com>
2022-03-11 10:11:46 -0300

    Including class: JSON response

--- Curso/NodeJS (Express) e MongoDB/to-do-list/app.js
+++ Curso/NodeJS (Express) e MongoDB/to-do-list/app.js
@@ -6,6 +6,10 @@ app.get('/', (request, response) => {
   response.send("<h1>Minha Lista de Tarefas</h1>");
 })
 
+app.get('/json', (request, response) => {
+  response.json({title: 'Tarefa X', done: true});
+})
+
 app.listen(3000, () => {
   console.log('Servidor foi iniciado!');
 })
