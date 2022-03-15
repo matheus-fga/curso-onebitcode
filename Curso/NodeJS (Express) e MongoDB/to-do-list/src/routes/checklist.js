4d0a4d2d5c22e109b8a1a205e10d9d304a8ba9bb
Matheus Silva <matheussilva.fga@gmail.com>
2022-03-15 13:28:13 -0300

    Modifying PUT and DELETE routes to update and delete data from MongoDB in to-do-list project

--- Curso/NodeJS (Express) e MongoDB/to-do-list/src/routes/checklist.js
+++ Curso/NodeJS (Express) e MongoDB/to-do-list/src/routes/checklist.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   try {
     let checklists = await Checklist.find({});
-    res.status(200).json(checklist);
+    res.status(200).json(checklists);
   } catch (error) {
     res.status(500).json(error);
   }
@@ -32,14 +32,23 @@ router.get('/:id', async (req, res) => {
   }
 })
 
-router.put('/:id', (req, res) => {
-  console.log(req.params.id);
-  res.send(`PUT ID: ${req.params.id}`);
+router.put('/:id', async (req, res) => {
+  try {
+    let { name } = req.body;
+    let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true});
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+  }
 })
 
-router.delete('/:id', (req, res) => {
-  console.log(req.params.id);
-  res.send(`DELETE ID: ${req.params.id}`);
+router.delete('/:id', async (req, res) => {
+  try {
+    let checklist = await Checklist.findByIdAndDelete(req.params.id);
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+  }
 })
 
 module.exports = router;
