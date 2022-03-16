02769246755f5866f09d8548073af49a0ab98121
Matheus Silva <matheussilva.fga@gmail.com>
2022-03-16 13:54:00 -0300

    Changing routes to pass data from the DB to views in to-do-list project

--- Curso/NodeJS (Express) e MongoDB/to-do-list/src/routes/checklist.js
+++ Curso/NodeJS (Express) e MongoDB/to-do-list/src/routes/checklist.js
@@ -6,9 +6,9 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   try {
     let checklists = await Checklist.find({});
-    res.status(200).json(checklists);
+    res.status(200).render('checklists/index', {checklists: checklists});
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).render('pages/error', {error: error});
   }
 })
 
@@ -26,9 +26,9 @@ router.post('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     let checklist = await Checklist.findById(req.params.id);
-    res.status(200).json(checklist);
+    res.status(200).render('checklists/show', {checklist: checklist});
   } catch (error) {
-    res.status(422).json(error);
+    res.status(500).render('pages/error', {error: error});
   }
 })
 
