0ca8ffce06783392279def987266a92b3e419992
Matheus Silva <matheussilva.fga@gmail.com>
2022-03-17 13:01:34 -0300

    Adding package method-override to to-do-list project

--- Curso/NodeJS (Express) e MongoDB/to-do-list/app.js
+++ Curso/NodeJS (Express) e MongoDB/to-do-list/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const checklistRouter = require('./src/routes/checklist');
 const rootRouter = require('./src/routes/index');
+const methodOverride = require('method-override');
 
 require('./config/database');
 
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
